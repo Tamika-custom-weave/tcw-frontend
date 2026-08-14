e268264bb3baa488457d5d9b9cf0fd29915f483f
ABODUNRIN IBRAHIM <159398228+Huncho6@users.noreply.github.com>
2026-08-14 22:34:42 +0100

    Add API proxy for production

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.38.23.165:5000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
